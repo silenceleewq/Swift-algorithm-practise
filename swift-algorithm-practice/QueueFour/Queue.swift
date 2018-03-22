751975a54efbb2032fa4b55ae8c1c1a104b38019
李仁强 <leonard.li@zhan.com>
2018-03-22 20:46:27 +0800

    add Queue proj

---
+++ swift-algorithm-practice/QueueFour/Queue.swift
@@ -0,0 +1,9 @@
+//
+//  Queue.swift
+//  QueueFour
+//
+//  Created by lirenqiang on 22/03/2018.
+//  Copyright © 2018 李仁强. All rights reserved.
+//
+
+import Foundation
---
+++ swift-algorithm-practice/QueueFour/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  QueueFour
+//
+//  Created by lirenqiang on 22/03/2018.
+//  Copyright © 2018 李仁强. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
